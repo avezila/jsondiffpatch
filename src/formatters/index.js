1eaf97c9930637d9987dbd416061d6dfd8ebb913
n.e0h <thepioh@zoho.com>
2016-12-21 16:48:15 +0300

    Update index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -5,7 +5,7 @@ exports.html = require('./html');
 exports.annotated = require('./annotated');
 exports.jsonpatch = require('./jsonpatch');
 
-if (!environment.isBrowser) {
-  var consoleModuleName = './console';
-  exports.console = require('./console');
-}
+// if (!environment.isBrowser) {
+var consoleModuleName = './console';
+exports.console = require('./console');
+// }
